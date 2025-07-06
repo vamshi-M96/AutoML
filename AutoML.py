ca5813ad0c8813322c11484e8a2ff2d980c25e26
vamshi-M96 <vamsi.meka96@gmail.com>
2025-07-06 23:25:19 +0530

    Update AutoML.py

--- AutoML.py
+++ AutoML.py
@@ -94,8 +94,8 @@ st.markdown(
 
 with st.expander("ℹ️ How to Use the App"):
     st.markdown("""
-    📂 **Upload data** in CSV/XLSX format.
-    🪛 Perform EDA & preprocessing.
+    -📂 **Upload data** in CSV/XLSX format.
+    -🪛 Perform EDA & preprocessing.
      🪡Select task (Classification/Regression).
     🚃 Train models and download best.
     🔮 Make predictions using manual input or batch file.
